1bd06baa7f7a8cae71c8bb5757f8de96b91546b5
Aironelite <aaronelias24@hotmail.com>
2025-06-24 17:38:23 +0000

    listo

--- src/app.js
+++ src/app.js
@@ -1,3 +1,35 @@
 //write your code here
 console.log("Hello Rigo from the console!");
+function pcoptions() {
+    const options = ["piedra", "papel", "tijera", "lagarto", "spock"];
+    const random = Math.floor(Math.random() * options.length);
+    return options[random];
+}
+
+function thewinner(player, pc) {
+    if (player === pc) {
+        return "¡VAYA EMPATE!";
+    }
+    const winnerplayer = {
+        piedra: ["tijera", "lagarto"],
+        papel: ["piedra", "spock"],
+        tijera: ["papel", "lagarto"],
+        lagarto: ["spock", "papel"],
+        spock: ["tijera", "piedra"]
+    };
+
+    if (winnerplayer[player].includes(pc)) {
+        return `Ganaste! ${player} extermino a ${pc}!!!!`;
+    } else {
+        return `¡Perdiste...! ${pc} extermino a ${player}!!!!`;
+    }
+}
+
+const player = prompt("Elige!: piedra, papel, tijera, lagarto o spock").toLowerCase();
+const pc = pcoptions();
+
+console.log("Player elige!:", player);
+console.log("Player 2 elige!:", pc);
+console.log(thewinner(player,pc));
+
 
